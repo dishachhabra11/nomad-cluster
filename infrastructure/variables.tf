db790557937de2fd2b55881c330f20d19795dc82
Deqode <deqode@Deqodes-MacBook-Pro.local>
2025-12-11 17:49:53 +0530

    removed var

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,9 +2,5 @@ variable "project_id" {
   description = "The GCP project ID to deploy resources into"
   type        = string
 }
-variable "nomad_client_secret" {
-  description = "oauth2_client_secret"
-  type        = string
-  sensitive = true
-}
+
 
