5f5143f43091c8a858e9956855964ab2e4e5e79b
Deqode <deqode@Deqodes-MacBook-Pro.local>
2025-12-12 17:24:39 +0530

    iap settings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -144,7 +144,7 @@ resource "google_compute_backend_service" "nomad_backend" {
 
 ## ----------------------  ssl_certificates
 
-resource "google_compute_managed_ssl_certificate" "nomad_cert_01" { 
+resource "google_compute_managed_ssl_certificate" "nomad_cert_new" { 
   
   # This is the name used in GCP, which is now managed by Terraform
   name = "nomad-cert-new" 
@@ -172,7 +172,7 @@ resource "google_compute_url_map" "nomad_lb_urlmap" {
 resource "google_compute_target_https_proxy" "nomad_lb_proxy" {
   name    = "nomad-lb-proxy"
   url_map = google_compute_url_map.nomad_lb_urlmap.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.nomad_cert_01.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.nomad_cert_new.self_link
 ]
     depends_on = [
      google_compute_url_map.nomad_lb_urlmap
