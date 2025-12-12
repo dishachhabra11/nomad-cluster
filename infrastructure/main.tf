9195146e094305112f479fba8fa83c1d6ccf5c66
Deqode <deqode@Deqodes-MacBook-Pro.local>
2025-12-12 16:08:21 +0530

    subdomain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -172,7 +172,7 @@ resource "google_compute_url_map" "nomad_lb_urlmap" {
 resource "google_compute_target_https_proxy" "nomad_lb_proxy" {
   name    = "nomad-lb-proxy"
   url_map = google_compute_url_map.nomad_lb_urlmap.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.nomad_cert_new.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.nomad_cert_01.self_link
 ]
     depends_on = [
      google_compute_url_map.nomad_lb_urlmap
