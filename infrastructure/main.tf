6578f9f8c0f423266600474a617c8d9bc546c39b
Deqode <deqode@Deqodes-MacBook-Pro.local>
2025-12-19 09:40:30 +0530

    added job

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,11 +16,11 @@ provider "google" {
   region  = "us-central1"
 }
 
-/* provider "nomad" {
-  address = "http://34.72.43.127:4646"
+ provider "nomad" {
+  address = "http://35.239.126.208:4646"
   region  = "us-central1"
 }
-*/
+
 
 ## --------------------------------------------------------------------------------------------------
 ## 2. Nomad Instance Template (for MIG)\
@@ -168,11 +168,11 @@ resource "google_compute_firewall" "client_firewall" {
 }
 
 ##----------  greptime job 
-/*
+
 resource "nomad_job" "greptime" {
   jobspec = file("${path.module}/jobs/greptime.nomad.hcl")
 }
-*/
+
 
 ## ------------ nomad client instance_template
 
