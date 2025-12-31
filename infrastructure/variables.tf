0a8d08e5c19a8f2265ac536e11fd5df2a1375747
disha-chhabra-0002 <dchhabra@deqode.com>
2025-12-31 12:45:27 +0530

    data blocks added

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,23 +3,3 @@ variable "project_id" {
   type        = string
   default    = "
 }
-
-variable "restic_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "aws_access_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "restic_repository" {
-  type    = string
-  default = "s3:s3.amazonaws.com/your-bucket-name"  # optional default
-}
