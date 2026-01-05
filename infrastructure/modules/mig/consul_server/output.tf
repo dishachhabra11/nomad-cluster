b6053fcc22dbfbfcb1d6b0dd0d9ea6782e8745ff
disha-chhabra-0002 <dchhabra@deqode.com>
2026-01-05 19:46:40 +0530

    debuigged

--- infrastructure/modules/mig/consul_server/output.tf
+++ infrastructure/modules/mig/consul_server/output.tf
@@ -1,9 +1,9 @@
 output "mig_name" {
-  value = google_compute_region_instance_group_manager.wazuh-server-mig.name
+  value = google_compute_region_instance_group_manager.consul-server-mig.name
 }
 
 output "instance_group_self_link" {
-  value = google_compute_region_instance_group_manager.wazuh-server-mig.instance_group
+  value = google_compute_region_instance_group_manager.consul-server-mig.instance_group
 }
 
 
